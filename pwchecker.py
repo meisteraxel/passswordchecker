fb6493cd4dc33b3c6423791da0d091302567e99f
Axel <meister.axel@gmail.com>
2023-06-16 22:28:07 +0200

    Add GUI

--- pwchecker.py
+++ pwchecker.py
@@ -1,53 +1,81 @@
 #Password Checker
 import string
+import tkinter
+import customtkinter
 
-password = input("Type your password! ")
-score = 0
+
+#Set TK Window
+customtkinter.set_appearance_mode("System")
+app = customtkinter.CTk()
+app.geometry("800x500")
+app.title("Password Check")
+app.grid_columnconfigure(0, weight=1)
+
 
 #Check the password for specific characters
-lowercase = False
-uppercase = False
-punctuation = False
-number = False
-lenght = False
 
-#Check for Characters
-for character in password:
-    if character in "abcdefghijklmnopqrstuvwxyz":
-        lowercase = True
-    elif character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        uppercase = True
-    elif character in "0123456789":
-        number = True
-    elif character in string.punctuation:
-        punctuation = True
-
-#Atleast 8 Characters long
-if len(password) > 8:
-    lenght = True
-else:
+def checkPassword():
+    password = pwbox.get()
+    print(password)
+    score = 0
+    
+    lowercase = False
+    uppercase = False
+    punctuation = False
+    number = False
     lenght = False
+    score = 0
+
+    #Check for Characters
+    for character in password:
+        if character in "abcdefghijklmnopqrstuvwxyz":
+            lowercase = True
+        elif character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            uppercase = True
+        elif character in "0123456789":
+            number = True
+        elif character in string.punctuation:
+            punctuation = True
+
+    #Atleast 8 Characters long
+    if len(password) > 8:
+        lenght = True
+    else:
+        lenght = False
 
 
-if lowercase  == True:
-    print("Your password contains lowercase characters.")
-if uppercase  == True:
-    print("Your password contains uppercase characters.")
-if punctuation == True:
-    print("Your password contains Punctuations.")
-if number == True:
-    print("Your password contains a number.")
-if lenght == True:
-    print("Your password is at least 8 characters long.")
+    if lowercase  == True:
+        print("Your password contains lowercase characters.")
+    if uppercase  == True:
+        print("Your password contains uppercase characters.")
+    if punctuation == True:
+        print("Your password contains Punctuations.")
+    if number == True:
+        print("Your password contains a number.")
+    if lenght == True:
+        print("Your password is at least 8 characters long.")
 
-#Score 
-if lowercase and uppercase == True:
-    score = score + 10
-if lowercase and uppercase and number == True:
-    score = score + 10
-if punctuation == True:
-    score = score + 5
-if lenght == True:
-    score = score + 5
+    #Score 
+    if lowercase and uppercase == True:
+        score = score + 10
+    if lowercase and uppercase and number == True:
+        score = score + 10
+    if punctuation == True:
+        score = score + 5
+    if lenght == True:
+        score = score + 5
 
-print("You achieved " + str(score) + "/30 points!")
+    print("You achieved " + str(score) + "/30 points!")
+
+
+#Define Buttons/Labels
+label = customtkinter.CTkLabel(app, text="Password Checker - created by Axel", fg_color="transparent")
+label.grid(row=0, column=0, padx=20, pady=20)
+
+pwbox = customtkinter.CTkEntry(master=app, placeholder_text="Password")
+pwbox.grid(row=1, column=0, padx=5, pady=5)
+
+analyze_button = customtkinter.CTkButton(app, text="Analyze PW", command=checkPassword)
+analyze_button.grid(row=2, column=0, padx=20, pady=20)
+
+app.mainloop()
