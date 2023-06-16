a8c8784f29fa070bb7c5b4472228c1d627d2a4ef
Axel <meister.axel@gmail.com>
2023-06-16 21:27:52 +0200

    Check Punctuation

--- pwchecker.py
+++ pwchecker.py
@@ -1,9 +1,12 @@
 #Password Checker
+import string
 
-password = input("Type your password!")
+password = input("Type your password! ")
 score = 0
 
 #Check the password for specific characters
+
+#Lowercase
 lowercase = False
 for character in password:
     if character in "abcdefghijklmnopqrstuvwxyz":
@@ -11,3 +14,21 @@ for character in password:
 
 if lowercase  == True:
     print("Your password contains lowercase characters.")
+
+#Lowercase
+uppercase = False
+for character in password:
+    if character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        uppercase = True
+
+if uppercase  == True:
+    print("Your password contains uppercase characters.")
+
+#Punctuation Sign
+punctuation = False
+for character in password:
+    if character in string.punctuation:
+        punctuation = True
+
+if punctuation == True:
+    print("Your password contains Punctuations.")
