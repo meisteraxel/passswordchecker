ed02672aa3906a1abee54385c95e4f95af6e4724
Axel <meister.axel@gmail.com>
2023-06-16 21:56:36 +0200

    Point System

--- pwchecker.py
+++ pwchecker.py
@@ -5,41 +5,49 @@ password = input("Type your password! ")
 score = 0
 
 #Check the password for specific characters
-
-#Lowercase
 lowercase = False
+uppercase = False
+punctuation = False
+number = False
+lenght = False
+
+#Check for Characters
 for character in password:
     if character in "abcdefghijklmnopqrstuvwxyz":
         lowercase = True
-
-if lowercase  == True:
-    print("Your password contains lowercase characters.")
-
-#Lowercase
-uppercase = False
-for character in password:
-    if character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    elif character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
         uppercase = True
-
-if uppercase  == True:
-    print("Your password contains uppercase characters.")
-
-#Punctuation Sign
-punctuation = False
-for character in password:
-    if character in string.punctuation:
+    elif character in "0123456789":
+        number = True
+    elif character in string.punctuation:
         punctuation = True
 
-if punctuation == True:
-    print("Your password contains Punctuations.")
-
 #Atleast 8 Characters long
-lenght = False
 if len(password) > 8:
     lenght = True
 else:
     lenght = False
 
+
+if lowercase  == True:
+    print("Your password contains lowercase characters.")
+if uppercase  == True:
+    print("Your password contains uppercase characters.")
+if punctuation == True:
+    print("Your password contains Punctuations.")
+if number == True:
+    print("Your password contains a number.")
 if lenght == True:
     print("Your password is at least 8 characters long.")
 
+#Score 
+if lowercase and uppercase == True:
+    score = score + 10
+if lowercase and uppercase and number == True:
+    score = score + 10
+if punctuation == True:
+    score = score + 5
+if lenght == True:
+    score = score + 5
+
+print("You achieved " + str(score) + "/30 points!")
